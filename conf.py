0e427a27822ef791a724fad7492198a3b1b0fbda
ken2s <ken2.sugimoto@gmail.com>
2022-12-18 17:26:56 +0900

    add index.md

---
+++ conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'test_document'
+copyright = '2022, ken2.s'
+author = 'ken2.s'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'cloud'
+html_static_path = ['_static']
+extensions = ['sphinxcontrib.seqdiag']
+extensions = ["myst_parser"]
+
